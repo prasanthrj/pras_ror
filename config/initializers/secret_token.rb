2fe8a3eba9a84e3ab336e272cf4a462c0892fae0
prasanth R <prasanth.r@imaginea.com>
2013-11-16 07:52:55 +0530

    Pras RoR Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PrasRor::Application.config.secret_token = 'f8d8f22f0e5e630acde961f142ed15dd40e5d11680d59e7e2fdf526a605539448d60a55ebfeefa14a44e9010ef5740203125ca49a64b1a11800c23d3c07904d3'
