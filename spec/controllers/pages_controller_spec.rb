6722f56db3f7671e4c70361de49420f812f7c559
prasanthrj <prasanth.ratakonda@yahoo.com>
2013-11-21 15:52:14 +0530

    Added tests to ror

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -2,11 +2,16 @@ require 'spec_helper'
 
 describe PagesController do
 
+render_views
   describe "GET 'home'" do
     it "returns http success" do
       get 'home'
       response.should be_success
     end
+	it "Should have right title" do
+	get 'home'
+	response.should have_selector("title", :content=> "Ruby on Rails sample App | Home ")
+	end
   end
 
   describe "GET 'contact'" do
